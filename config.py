4b7d12fd33c38b86a74724fe3c3a51c006609443
Ax3L <okateer@protonmail.com>
2021-08-16 15:47:54 +0300

    added argparser and delete function

--- config.py
+++ config.py
@@ -3,6 +3,5 @@ import sys
 from pathlib import Path
 
 DEFAULT_CONFIG = {
-    "CHANNEL_URL":  sys.argv[1],
     "NEWSBOAT_URLS_FILE": str(Path.home()) + "/.newsboat/urls",
 }
--- newsboat_youtube_add.py
+++ newsboat_youtube_add.py
@@ -7,12 +7,31 @@ Add Youtube channel to Newsboat URLs:
 import config
 import re
 
-CHANNEL_URL = config.DEFAULT_CONFIG["CHANNEL_URL"]
+import argparse
+
+
+parser = argparse.ArgumentParser(
+    description="Add or delete Youtube channel from Newsboat urls file."
+)
+parser.add_argument(
+    "CHANNEL_URL",
+    metavar="url",
+    type=str,
+    help="YouTube channel, for example: https://www.youtube.com/channel/UCUZHFZ9jIKrLroW8LcyJEQQ",
+)
+parser.add_argument("-a", "--add", action="store_true", help="add channel to urls")
+parser.add_argument(
+    "-d", "--delete", action="store_true", help="delete channel from urls"
+)
+args = parser.parse_args()
+
+
+CHANNEL_URL = args.CHANNEL_URL
 NEWSBOAT_URLS_FILE = config.DEFAULT_CONFIG["NEWSBOAT_URLS_FILE"]
 
 
 def validate_channel_id(channel_id) -> None:
-    """ Validate Channel ID by regexp """
+    """Validate Channel ID by regexp"""
     string = channel_id
     pattern = re.compile("[0-9A-Za-z_-]{21}[AQgw]")
     match = pattern.search(string)
@@ -22,21 +41,25 @@ def validate_channel_id(channel_id) -> None:
 
 
 def get_channel_id() -> str:
-    """ Return channel id from Youtube channel URL."""
-    channel_id = CHANNEL_URL.split("/")[4]
-    validate_channel_id(channel_id)
+    """Return channel id from Youtube channel URL."""
+    try:
+        channel_id = CHANNEL_URL.split("/")[4]
+        validate_channel_id(channel_id)
+    except IndexError as e:
+        print(f"Incorrect Youtube channel URL: {e}")
+        exit()
     return channel_id
 
 
 def create_rss_link() -> str:
-    """ Generate Newsboat compatible RSS link. """
+    """Generate Newsboat compatible RSS link."""
     template_url = "https://www.youtube.com/feeds/videos.xml?channel_id="
     channel_id = get_channel_id()
     return template_url + channel_id
 
 
 def check_already_exists(newsboat_url: str) -> None:
-    """ Exit if channel id already exists in newsboat urls. """
+    """Exit if channel id already exists in newsboat urls."""
     with open(NEWSBOAT_URLS_FILE, "r") as file:
         for line in file:
             if newsboat_url in line:
@@ -46,22 +69,44 @@ def check_already_exists(newsboat_url: str) -> None:
 
 
 def get_count_newsboat_urls() -> int:
-    """ Return number of lines from URLs file. """
+    """Return number of lines from URLs file."""
     num_lines = sum(1 for line in open(NEWSBOAT_URLS_FILE))
     return num_lines
 
 
 def add_newsboat_url(newsboat_url: str) -> None:
-    """ Add correct newsboat url with Youtube channel. """
+    """Add correct newsboat url with Youtube channel."""
     check_already_exists(newsboat_url)
     with open(NEWSBOAT_URLS_FILE, "a") as file:
         data = newsboat_url
-        file.write(data + '\n')
+        file.write(data + "\n")
     count = get_count_newsboat_urls()
     print(f"[Added] URL to Newsboat list. Now {count} URLs in file.")
 
 
-if __name__ == "__main__":
+def delete_newsboat_url(newsboat_url: str) -> None:
+    """Delete Youtube channel from newsboat urls"""
+    with open(NEWSBOAT_URLS_FILE, "r") as file:
+        filedata = file.read()
+
+    data = newsboat_url
+    filedata = filedata.replace(data + "\n", "")
+
+    with open(NEWSBOAT_URLS_FILE, "w") as file:
+        file.write(filedata)
+
+    count = get_count_newsboat_urls()
+    print(f"[Deleted] URL from Newsboat list. Now {count} URLs in file.")
+
+
+def main():
     newsboat_url = create_rss_link()
     print(f"[URL] {newsboat_url}")
-    add_newsboat_url(newsboat_url)
+    if args.add is True:
+        add_newsboat_url(newsboat_url)
+    elif args.delete is True:
+        delete_newsboat_url(newsboat_url)
+
+
+if __name__ == "__main__":
+    main()
